743789aff901d0f502c19e0c1f61b15ee92e1bc0
Seiji Ralph Villafranca <seijiv13@outlook.com>
2020-03-06 21:15:47 +0800

    added cors for client side accessing

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import * as http from 'http';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import routes from './routes';
+import * as cors from 'cors';
 createConnection().then(async connection => {
    const app = express();
+   app.use(cors());
    app.use(bodyParser.json());
    app.use('/', routes);
 
